87e079c1350f8a6f907057d3375c0d6e0fe12f3a
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-05-12 14:12:51 +0200

    Add fixes

--- server.mjs
+++ server.mjs
@@ -1,21 +1,15 @@
 import jsonServer from 'json-server';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'build/db/app.json'));
+const router = jsonServer.router('build/db/app.json');
 const middlewares = jsonServer.defaults({
-  static: path.join(__dirname, 'build'),
-  noCors: true,
+  static: 'build',
+  noCors: true
 });
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3131;
 
 server.use(middlewares);
 server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
+  '/api/*': '/$1'
 }));
 server.use(router);
 server.listen(port);
